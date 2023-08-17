055d5fd2f24c2ffa737c205837a1310d17acc591
Thomas Colin <thomas.a.colin@gmail.com>
2023-08-18 09:24:19 +1000

    hinge vis updates

--- d3-hinge-vis/relationships.js
+++ d3-hinge-vis/relationships.js
@@ -2,13 +2,13 @@ const people = {};
 let time_so_far = 0;
 let speed = 300;
 const radius = 3,
-  padding = 1,
-  cluster_padding = 5;
+  padding = 2,
+  cluster_padding = 2;
 
 const margin = { top: 0, right: 20, bottom: 20, left: 20 },
   width = 600 - margin.left - margin.right,
   height = 600 - margin.top - margin.bottom;
-const rowWidth = 8;
+const rowWidth = 7;
 const columnWidth = 2;
 
 const svg = d3
@@ -20,33 +20,10 @@ const svg = d3
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 d3.select("#chart").style("width", width + margin.left + margin.right + "px");
 
-svg
-  .append("text")
-  .attr("x", (width + margin.left + margin.right) / 2)
-  .attr("y", 30)
-  .attr("text-anchor", "middle")
-  .style("font-size", "29px")
-  .text("TIME AFTER SWIPING ON HINGE");
+const monthsCounter = d3.select("#monthsCounter");
 
-const iconOffset = 191;
-svg
-  .append("circle")
-  .attr("cx", width / 2 - iconOffset)
-  .attr("cy", 55)
-  .attr("r", 3)
-  .style("fill", "#8d5e86");
 
-svg
-  .append("text")
-  .attr("x", width / 2 + 20)
-  .attr("y", 60)
-  .attr("text-anchor", "middle")
-  .style("font-size", "13px")
-  .text(
-    "Every\u00A0\u00A0 represents one of the 1,036 women I had an interaction with on the app"
-  );
-
-  const arrowStartY = (2 * height) / rowWidth;
+  const arrowStartY = (1 * height) / rowWidth;
   const arrowEndY = height;
   
   svg.append("line")
@@ -82,97 +59,96 @@ svg
      .style("dominant-baseline", "middle")
      .style("writing-mode", "tb")  // This makes the text vertical
      .text("How far each interaction progressed");
-  
-
-
-const monthsField = svg
-  .append("text")
-  .attr("x", width / 2)
-  .attr("y", 95)
-  .attr("text-anchor", "middle")
-  .style("font-size", "30px");
 
 const groups = {
   "Pre-like status": {
     x: width / 2,
     y: -400,
-    color: "grey",
+    color: "#a9a9a9",
     cnt: 0,
     fullname: "Pre-like status",
   },
   "Pre-liked me status": {
     x: (width / 2) ,
     y: -400,
-    color: "grey",
+    color: "#a9a9a9",
     cnt: 0,
     fullname: "Pre-liked me status",
   },
   "Sent like": {
-    x: width / 3 - 30,
-    y: (2 * height) / rowWidth,
-    color: "grey",
+    x: width / 2 - 100,
+    y: (1 * height) / rowWidth,
+    color: "#a9a9a9",
     cnt: 0,
     fullname: "Sent like",
   },
   "Liked me": {
     x: (width / 3) * 2 + 30,
-    y: (2 * height) / rowWidth,
-    color: "grey",
+    y: (1 * height) / rowWidth,
+    color: "#a9a9a9",
     cnt: 0,
     fullname: "Liked me",
   },
   Match: {
     x: width / 2,
-    y: (3 * height) / rowWidth,
+    y: (2 * height) / rowWidth,
     color: "#8d5e86",
     cnt: 0,
     fullname: "Match",
   },
   "Conversation in app": {
     x: width / 2,
-    y: (4 * height) / rowWidth,
+    y: (3 * height) / rowWidth,
     color: "#8d5e86",
     cnt: 0,
     fullname: "Conversation in app",
   },
   "First date": {
     x: width / 2,
-    y: (5.0 * height) / rowWidth,
+    y: (4 * height) / rowWidth,
     color: "#FFA4B6", 
     cnt: 0,
     fullname: "First date",
   },
   "Second date": {
     x: width / 2,
-    y: (6 * height) / rowWidth,
+    y: (5 * height) / rowWidth,
     color: "#FFA4B6",
     cnt: 0,
     fullname: "Second date",
   },
   Dating: {
-    x: width / 2 - 80,
-    y: (7 * height) / rowWidth,
+    x: width / 2 - 100,
+    y: (6 * height) / rowWidth,
     color: "#FFA4B6",
     cnt: 0,
     fullname: "Dating",
   },
   Ended: {
     x: (width / 2) + 80,
-    y: (7 * height) / rowWidth,
+    y: (6 * height) / rowWidth,
     color: "black",
     cnt: 0,
     fullname: "Ended",
   },
   "Deleted the app": {
     x: (width / 2) ,
-    y: (8 * height) / rowWidth,
-    color: "red",
+    y: (7 * height) / rowWidth,
+    color: "#DE3163",
     cnt: 0,
     fullname: "Deleted the app",
   },
 };
+
+let cumulativeSentLike = 0;
+let cumulativeLikedMe = 0;
 let cumulativeMatches = 0;
+let cumulativeConversationInApp = 0;
 let cumulativeFirstDates = 0;
+let cumulativeSecondDates = 0;
+let cumulativeDating = 0;
+let cumulativeEnded = 0;
+let cumulativeDeleted = 0;
 
 function initializeSimulation() {
   const stages = d3.csv("stages_all.csv", d3.autoType);
@@ -291,9 +267,10 @@ function initializeSimulation() {
       .attr("text-anchor", (d) => (d === "Liked me" || d === "Ended" ? "end" : "left"))
       .attr("x", (d) => (d === "Liked me" || d === "Ended" ? width - 50 : 0))
       .attr("y", (d) => groups[d].y)
-      .text((d) => groups[d].fullname.toUpperCase());
+      .text((d) => groups[d].fullname.toUpperCase())
+      .attr("font-size", "18px");
 
-    const simulation = d3
+      const simulation = d3
       .forceSimulation(nodes)
       .force("x", (d) => d3.forceX(d.x))
       .force("y", (d) => d3.forceY(d.y))
@@ -308,12 +285,37 @@ function initializeSimulation() {
         .attr("cy", (d) => d.y)
         .attr("fill", (d) => groups[d.group].color);
 
-        if (simulation.alpha() < 0.01) {
-            simulation.stop();
-        }
+      if (simulation.alpha() < 0.01) {
+        simulation.stop();
+      }
     });
+    let isPaused = false; // To track if the simulation is paused
 
+    function resetButtonColors() {
+      document.getElementById("pause-button").style.backgroundColor = "white";
+      document.getElementById("slow-button").style.backgroundColor = "white";
+      document.getElementById("fast-button").style.backgroundColor = "white";
+
+      document.getElementById("pause-button").style.color = "black";
+      document.getElementById("slow-button").style.color = "black";
+      document.getElementById("fast-button").style.color = "black";
+    }
+    
+    document.getElementById("slow-button").style.backgroundColor = "black";
+    document.getElementById("slow-button").style.color = "white";
+    document
+      .getElementById("pause-button")
+      .addEventListener("click", function () {
+        resetButtonColors();
+        this.style.backgroundColor = "black";
+        this.style.color = "white";
+        isPaused = !isPaused; // Toggle the paused state
+        if (!isPaused) {
+          timer(); // If unpausing, restart the timer
+        }
+      });
     function timer() {
+      if (isPaused) return; // If paused, do not continue with the timer
       nodes.forEach(function (o, i) {
         o.timeleft -= 1;
         if (o.timeleft == 0 && o.istage < o.stages.length - 1) {
@@ -327,29 +329,125 @@ function initializeSimulation() {
         }
       });
       nodes.forEach(function (node, i) {
-        if (
-          node.group === "Match" &&
-          (node.prevGroup === "Sent like" || node.prevGroup === "Liked me")
-        ) {
-          cumulativeMatches++;
+        switch (node.group) {
+            case "Sent like":
+                if (node.prevGroup === "Pre-like status") {
+                    cumulativeSentLike++;
+                }
+                break;
+            case "Liked me":
+                if (node.prevGroup === "Pre-liked me status") { // Add your condition here if specific.
+                    cumulativeLikedMe++;
+                }
+                break;
+            case "Match":
+                if (node.prevGroup === "Sent like" || node.prevGroup === "Liked me") {
+                    cumulativeMatches++;
+                }
+                break;
+            case "Conversation in app":
+                if (node.prevGroup === "Match") { // Adjust this condition if necessary.
+                    cumulativeConversationInApp++;
+                }
+                break;
+            case "First date":
+                if (node.prevGroup === "Conversation in app") {
+                    cumulativeFirstDates++;
+                }
+                break;
+            case "Second date":
+                if (node.prevGroup === "First date") { // Adjust this condition if necessary.
+                    cumulativeSecondDates++;
+                }
+                break;
+            case "Dating":
+                if (node.prevGroup === "Second date") { // Adjust this condition if necessary.
+                    cumulativeDating++;
+                }
+                break;
+            case "Ended":
+              if (node.prevGroup === "Dating") { // Adjust this condition if necessary.
+                    cumulativeEnded++;
+                }
+                break;
+            case "Deleted the app":
+              if (node.prevGroup === "Dating") { // Adjust this condition if necessary.
+                    cumulativeDeleted++;
+                }
+                break;
         }
-        if (
-          node.group === "First date" &&
-          node.prevGroup === "Conversation in app"
-        ) {
-          cumulativeFirstDates++;
-        }
+    
         node.prevGroup = node.group;
-      });
+    });
+    
+    svg.selectAll(".grp").each(function(d) {
+      let textElement = d3.select(this);
+      let mainText;
+      let numberText;
+      switch (d) {
+          case "Sent like":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeSentLike}`;
+              break;
+          case "Liked me":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeLikedMe}`;
+              break;
+              case "Match":
+                mainText = `${groups[d].fullname.toUpperCase()} (`;
+                numberText = `${cumulativeMatches}`;
+                break;
+
+                case "Conversation in app":
+                  mainText = `${groups[d].fullname.toUpperCase()} (`;
+                  numberText = `${cumulativeConversationInApp}`;
+                  break;
+                  case "Liked me":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeLikedMe}`;
+              break;
+              case "First date":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeFirstDates}`;
+              break;
+              case "Second date":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeSecondDates}`;
+              break;
+              case "Dating":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeDating}`;
+              break;
+              case "Ended":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeEnded}`;
+              break;
+              case "Deleted the app":
+              mainText = `${groups[d].fullname.toUpperCase()} (`;
+              numberText = `${cumulativeDeleted}`;
+              break;          
+          default:
+              textElement.text(groups[d].fullname.toUpperCase());
+              return;  // exit early for the default case
+      }
+      textElement.text(mainText)
+          .append("tspan")
+          .attr("font-weight", "bold")
+          .text(numberText)
+          .append("tspan")
+          .attr("font-weight", "normal")
+          .text(")");
+  });
+    
 
       time_so_far += 1;
       var currdate = new Date(startdate.getTime() + time_so_far * 86400000);
       xScale.domain([startdate, currdate]);
-      lineSvg.select("g")
-      .transition()
-      .duration(300)  // You can adjust this duration as needed
-      .call(xAxis);
-
+      lineSvg
+        .select("g")
+        .transition()
+        .duration(300) // You can adjust this duration as needed
+        .call(xAxis);
 
       d3.select("#timecount .cnt").text(time_so_far);
 
@@ -357,11 +455,21 @@ function initializeSimulation() {
       document
         .getElementById("slow-button")
         .addEventListener("click", function () {
-          speed = 600;
-        });      
+          resetButtonColors();
+          this.style.backgroundColor = "black";
+          this.style.color = "white";
+          if (isPaused) {
+            isPaused = !isPaused; // Toggle the paused state
+            timer(); // If unpausing, restart the timer
+          }
+          speed = 500;
+        });
       document
         .getElementById("fast-button")
         .addEventListener("click", function () {
+          resetButtonColors();
+          this.style.backgroundColor = "black";
+          this.style.color = "white";
           speed = 50;
         });
 
@@ -375,7 +483,7 @@ function initializeSimulation() {
       });
 
       yScale.domain([0, d3.max(lineDataMatches, (d) => cumulativeMatches)]);
-      
+
       lineSvg.select(".y-axis").call(yAxis);
 
       xScale.domain(d3.extent(lineDataMatches, (d) => d.date));
@@ -383,7 +491,7 @@ function initializeSimulation() {
       const pathMatches = lineSvg
         .selectAll(".line-path-matches")
         .data([lineDataMatches]);
-      
+
       pathMatches
         .enter()
         .append("path")
@@ -399,20 +507,22 @@ function initializeSimulation() {
 
       const lastMatchData = lineDataMatches[lineDataMatches.length - 1];
 
-    const matchLabel = lineSvg.selectAll(".line-label-matches")
+      const matchLabel = lineSvg
+        .selectAll(".line-label-matches")
         .data([lastMatchData]);
 
-        matchLabel.enter()
+      matchLabel
+        .enter()
         .append("text")
         .attr("class", "line-label-matches")
         .merge(matchLabel)
-        .attr("x", d => lineWidth - lineRightMargin + 10) // +5 for a small right offset
-        .attr("y", d => lineTopMargin)        
-        .text(d => `💜${d.cumulativeCount} matches`);
+        .attr("x", (d) => lineWidth - lineRightMargin + 10) // +5 for a small right offset
+        .attr("y", (d) => lineTopMargin)
+        .text((d) => `💜${d.cumulativeCount} matches`);
 
-        matchLabel.exit().remove();
-      
-        lineDataFirstDates.push({
+      matchLabel.exit().remove();
+
+      lineDataFirstDates.push({
         date: currdate,
         cumulativeCount: cumulativeFirstDates,
       });
@@ -433,22 +543,25 @@ function initializeSimulation() {
         .attr("d", line);
 
       pathFirstDates.exit().remove();
-      const lastFirstDateData = lineDataFirstDates[lineDataFirstDates.length - 1];
+      const lastFirstDateData =
+        lineDataFirstDates[lineDataFirstDates.length - 1];
 
-      const firstDateLabel = lineSvg.selectAll(".line-label-first-dates")
+      const firstDateLabel = lineSvg
+        .selectAll(".line-label-first-dates")
         .data([lastFirstDateData]);
-      
-      firstDateLabel.enter()
+
+      firstDateLabel
+        .enter()
         .append("text")
         .attr("class", "line-label-first-dates")
         .merge(firstDateLabel)
-        .attr("x", d => lineWidth - lineRightMargin + 10) // +5 for a small right offset
-        .attr("y", d => yScale(d.cumulativeCount))
-        //.attr("dy", "-0.15em") // slight upwards offset        
-        .text(d => `🍸${d.cumulativeCount} first dates`);
-      
+        .attr("x", (d) => lineWidth - lineRightMargin + 10) // +5 for a small right offset
+        .attr("y", (d) => yScale(d.cumulativeCount))
+        //.attr("dy", "-0.15em") // slight upwards offset
+        .text((d) => `🍸${d.cumulativeCount} first dates`);
+
       firstDateLabel.exit().remove();
-      const monthsElapsed = Math.floor(time_so_far / 30.44);
+      
       function daysToMonthsAndDays(days) {
         const averageMonthLength = 30.44;
         let months = Math.floor(days / averageMonthLength);
@@ -458,8 +571,8 @@ function initializeSimulation() {
 
       let elapsedDays = time_so_far;
       let result = daysToMonthsAndDays(elapsedDays);
-
-      monthsField.text(`${result.months} months, ${result.days} days`);
+      
+      monthsCounter.text(`${result.months} months, ${result.days} days`);
     }
 
     d3.timeout(timer, 500);
@@ -530,4 +643,5 @@ function forceCollide() {
 
   return force;
 }
+
 initializeSimulation();
